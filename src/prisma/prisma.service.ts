34d7fc1bd5b3b8613027871d825343fde30d4d8b
MoizShah-227 <103181576+MoizShah-227@users.noreply.github.com>
2026-01-19 17:47:43 +0500

    Did some change in prisma.service

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,23 +1,30 @@
-import { Injectable } from '@nestjs/common';
-import {PrismaClient}  from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';  
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
-  User: any;
-  constructor(config:ConfigService) {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor(config: ConfigService) {
     const adapter = new PrismaPg({
-      connectionString: config.get("DATABASE_URL"), 
+      connectionString: config.get<string>('DATABASE_URL'),
     });
-    super({ adapter }); 
+    super({ adapter });
   }
 
-  cleanDb(){
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+
+  async cleanDb() {
     return this.$transaction([
       this.subtasks.deleteMany({}),
       this.tasks.deleteMany({}),
       this.user.deleteMany({}),
-    ])
+    ]);
   }
 }
