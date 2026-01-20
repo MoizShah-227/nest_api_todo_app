7e2bdec89dea02a1d58e339aba97fe6858cd4269
MoizShah-227 <103181576+MoizShah-227@users.noreply.github.com>
2026-01-20 14:50:06 +0500

    add example to the auth dto

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty} from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class AuthDto {
   @IsEmail()
   @IsNotEmpty()
+  @ApiProperty({example:"syedmoiz123@gmail.com"})
   email!: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({example:"123"})
   password!: string;
 }
