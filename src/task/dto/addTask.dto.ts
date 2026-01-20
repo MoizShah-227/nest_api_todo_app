355e706796386b49ae46a89b285b3f0485c788b1
MoizShah-227 <103181576+MoizShah-227@users.noreply.github.com>
2026-01-20 15:01:10 +0500

    add example data to dto

--- src/task/dto/addTask.dto.ts
+++ src/task/dto/addTask.dto.ts
@@ -1,24 +1,36 @@
 import { IsString, IsBoolean, IsOptional, ValidateNested, ArrayMinSize } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 
 export class CreateSubtaskDto {
+  @ApiProperty({ example: 'Subtask-1' })
   @IsString()
   title!: string;
 
+  @ApiPropertyOptional({ example: false })
   @IsBoolean()
   @IsOptional()
   completed?: boolean;
 }
 
 export class CreateTaskDto {
+  @ApiProperty({ example: 'Task-1' })
   @IsString()
   title!: string;
 
+  @ApiPropertyOptional({ example: false })
   @IsBoolean()
   @IsOptional()
   completed?: boolean;
 
+  @ApiPropertyOptional({
+    type: () => [CreateSubtaskDto],
+    example: [
+      { title: 'Subtask-1', completed: false },
+      { title: 'Subtask-2',completed: false}
+    ],
+  })
   @IsOptional()
   @ValidateNested({ each: true })
   @Type(() => CreateSubtaskDto)
@@ -28,14 +40,17 @@ export class CreateTaskDto {
 export class EditTaskDto {
   @IsOptional()
   @IsString()
+  @ApiProperty({example:"Task-1"})
   title?: string;
 
   @IsOptional()
   @IsBoolean()
+  @ApiProperty({example:true})
   completed?: boolean;
 
   @IsOptional()
   @ValidateNested({ each: true })
   @Type(() => CreateSubtaskDto)
+  @ApiProperty({example:[{"title":"sub-task-1"}]})
   subtasks?: CreateSubtaskDto[];
 }
