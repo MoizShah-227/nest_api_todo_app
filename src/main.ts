089813e65de896bec1dad137e22c79d74106fd72
MoizShah-227 <103181576+MoizShah-227@users.noreply.github.com>
2026-01-20 10:48:08 +0500

    add port in connection in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -21,10 +22,12 @@ async function bootstrap() {
     .addBearerAuth() 
     .build();
 
+  const configService = app.get(ConfigService);
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('', app, document);
-
-  await app.listen(4444);
+  const port =configService.get<number>('PORT') || 4444;
+  console.log(port)
+  await app.listen(port);
 }
 
 bootstrap();
